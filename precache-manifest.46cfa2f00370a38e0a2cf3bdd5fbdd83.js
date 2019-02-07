0ed5acf29d080a3b3d8c75c448b5e110b76c67d9
Bodia Valeur <bodiavaleur@gmail.com>
2019-02-07 13:27:29 +0200

    Updates

---
+++ precache-manifest.46cfa2f00370a38e0a2cf3bdd5fbdd83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "668bde0d4f36d7c8d3a0",
+    "url": "/bodiavaleur/diamond/static/css/main.ec0104d5.chunk.css"
+  },
+  {
+    "revision": "668bde0d4f36d7c8d3a0",
+    "url": "/bodiavaleur/diamond/static/js/main.668bde0d.chunk.js"
+  },
+  {
+    "revision": "f370b753e5901661dd5d",
+    "url": "/bodiavaleur/diamond/static/js/1.f370b753.chunk.js"
+  },
+  {
+    "revision": "681a90d8469ead0db064",
+    "url": "/bodiavaleur/diamond/static/js/runtime~main.681a90d8.js"
+  },
+  {
+    "revision": "95db0654937328028ad1e9db85ca355a",
+    "url": "/bodiavaleur/diamond/index.html"
+  }
+];
